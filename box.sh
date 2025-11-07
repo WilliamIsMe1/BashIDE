47e4cda63830d568bff71b38ec33b9c93e9e8987
William <unbelievable.half.insane@gmail.com>
2025-11-07 16:47:53 +0000

    I'm implementing the boxes

---
+++ box.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONST_DOWN_RIGHT=$'\u250c'
+CONST_LEFT_DOWN_RIGHT=$'\u252c'
+CONST_LEFT_RIGHT=$'\u2500'
+CONST_LEFT_DOWN=$'\u2510'
+
+# https://en.wikipedia.org/wiki/Box-drawing_characters
+
+box(){ # x y width height
+
+}
--- main.sh
+++ main.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "This is BashIDE"
+clear
+tput cup 0 0
+echo Hello, World
+
+window_x_size=((tput lines))
+window_y_size=((tput rows))
+
+update_size_vars(){
+    export window_x_size=((tput lines))
+    export window_y_size=((tput rows))
+}
+
