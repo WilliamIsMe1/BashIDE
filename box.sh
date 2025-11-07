a0c760f7b78edf9ac6c14893d9bc0b04a270a82c
William <unbelievable.half.insane@gmail.com>
2025-11-07 19:32:43 +0000

    A brave attempt at simple boxes

--- box.sh
+++ box.sh
@@ -1,12 +1,30 @@
-#!/bin/bash
+CONST_DOWN_RIGHT=$'\u250c'      # ┌
+CONST_LEFT_DOWN_RIGHT=$'\u252c' # ┬
+CONST_LEFT_RIGHT=$'\u2500'      # ─
+CONST_LEFT_DOWN=$'\u2510'       # ┐
+CONST_UP_LEFT_DOWN=$'\u2524'    # ┤
+CONST_UP_DOWN=$'\u2502'         # │
+CONST_UP_RIGHT_DOWN=$'\u251c'   # ├
+CONST_CROSS=$'\u253c'           # ┼
+CONST_UP_LEFT=$'\u2518'         # ┘
+CONST_UP_LEFT_RIGHT=$'\u2534'   # ┴
+CONST_UP_RIGHT=$'\u2514'        # └
 
-CONST_DOWN_RIGHT=$'\u250c'
-CONST_LEFT_DOWN_RIGHT=$'\u252c'
-CONST_LEFT_RIGHT=$'\u2500'
-CONST_LEFT_DOWN=$'\u2510'
 
 # https://en.wikipedia.org/wiki/Box-drawing_characters
 
-box(){ # x y width height
-
+simple_box() { # x y width height char
+    local x=$1
+    local y=$2
+    local width=$3
+    local height=$4
+    local char=$5
+    local char_length=${#char}
+    if [ $char_length -gt 1 ]; then
+        return 'error'
+    fi
+    for i in {x..$(( x + width ))}; then
+        tput cup $i $y
+        echo char
+    done
 }
--- main.sh
+++ main.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+source ./box.sh
+
 clear
 tput cup 0 0
 echo Hello, World
 
-window_x_size=((tput lines))
-window_y_size=((tput rows))
+window_x_size=$(tput cols)
+window_y_size=$(tput lines)
 
 update_size_vars(){
-    export window_x_size=((tput lines))
-    export window_y_size=((tput rows))
+    export window_x_size=$(tput lines)
+    export window_y_size=$(tput rows)
 }
 
+simple_box 2 2 4 1 '#'
